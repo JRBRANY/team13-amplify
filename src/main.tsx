989a0b927bf27cf8c813e04512f9d5a4749af646
Noah Nisbet <nnisbet@clemson.edu>
2025-02-11 01:36:24 -0500

    adding more pages

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,7 @@ import { AuthProvider } from "react-oidc-context";
 const cognitoAuthConfig = {
   authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_jnOjoFtl2",
   client_id: "r7oq53d98cg6a8l4cj6o8l7tm",
+  client_secret: "g2os3g88a51u4fltin70j228m18d8l63a3349gmi8ga88j6jf04",
   redirect_uri: "https://main.d1zgxgaa1s4k42.amplifyapp.com/",
   response_type: "code",
   scope: "email openid phone",
