d52c70d18302e6b57c7d4b9e06a50e343f1399db
Noah Nisbet <nnisbet@clemson.edu>
2025-02-11 00:51:11 -0500

    adding more pages

--- src/pages/Driver.tsx
+++ src/pages/Driver.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export default function Driver() {
   return <h1>Driver Page</h1>;
 }
--- src/pages/Sponsor.tsx
+++ src/pages/Sponsor.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export default function Sponsor() {
   return <h1>Sponsor Page</h1>;
 }
